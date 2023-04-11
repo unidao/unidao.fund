fae22d263060824ec00d9f7dbddefe863b167608
Gleb Shishov <glebshishov@gmail.com>
2023-04-11 21:17:00 +0300

    Исправил хиро, исправил партнёров, переделал меню

--- js/main.js
+++ js/main.js
@@ -25,6 +25,33 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+//
+//
+// /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+//
+//
+// var slideIndex = 0;
+// carousel();
+//
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
 
 let slideIndex = 0;
 showSlides();
